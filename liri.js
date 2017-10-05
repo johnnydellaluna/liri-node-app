3e96b109cc870816dce3e6441c96404dda74a96e
Johnny Dellaluna <johnnydellaluna@gmail.com>
2017-10-05 17:50:46 -0400

    separating key files

--- liri.js
+++ liri.js
@@ -1,43 +1,74 @@
 // my-tweets
 
+var keys = require('keys.js');
+
 var Twitter = require('twitter');
 
-var client = new Twitter({
-  consumer_key: 'icvQRNiJ8GI9ua15jDFICB2r2',
-  consumer_secret: 'XG1kqxwWMiyF24OkzWNdVEE6nKqfm6h5hQh2mf0uJMnur2HZ6a',
-  access_token_key: '278884185-M6Wo3Yu0nN99ZWNidBwVVZobQPDnP7na7Vlz0JRW',
-  access_token_secret: 'wEWF49pJYP6MhusRRJDnVMvvN2eNaOwaYDhDgKyG9VLJm',
-});
+var Spotify = require('node-spotify-api');
 
-var params = {johnnydellaluna: 'nodejs'};
+var request = require("request");
+
+
+var params = {screen_name: 'johnnydellaluna'};
+
+var movieTitle =
+
+var songTitle = 
 
 // What we want are the text and created_at attributes
 
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    for(var i = 0; i < 20; i++) {
+    console.log(response);
   }
+}
 });
 
-
-
 // spotify-this-song
+// Get artist, song name, Spotify link, album
+ 
 
-
-
-
-
-
-
-
-
+function spotify() {
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+	console.log(data); 
+});
+};
 
 // movie-this
 
+request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short" + movieKey, function(error, response, body) {
 
-
-
-
-
+  if (!error && response.statusCode === 200) {
+    console.log("Title: ");
+    console.log("Year: ");
+    console.log("Rotten Tomatoes: ");
+    console.log("IMDB: " + JSON.parse(body).imdbRating);
+    console.log("Country: ");
+    console.log("Language: ");
+    console.log("Plot: ");
+    console.log("Actors: ");
+  }
+});
 
 // do-what-it-says
+
+var fs = require("fs");
+
+fs.readFile("random.txt", "utf8", function(error, data) {
+
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log(data);
+
+  var dataArr = data.split(",");
+
+  console.log(dataArr);
+
+});
+
